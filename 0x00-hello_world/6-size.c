26ea44a922bddf6101d6be6eced30d33f62fc9b0
Olaoluwa Abioje <abiojeolaoluwa59@gmail.com>
2022-09-08 21:38:53 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-	int x;
-	char z;
-	
+{	
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
