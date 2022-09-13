0f23c5a46a1d44c94d4c8045e995b3e77a0906dd
Olaoluwa Abioje <abiojeolaoluwa59@gmail.com>
2022-09-13 15:00:34 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar as message
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int c, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (c = 0; c < sz; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
