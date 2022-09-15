07664e18639dddd8480ff82a1a8c7803ac41dbc3
Olaoluwa Abioje <abiojeolaoluwa59@gmail.com>
2022-09-15 14:56:29 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a number from 0 to 9
+ * @c: input number
+ * REturn: 1 if number is from 0 to 9, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c<= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
